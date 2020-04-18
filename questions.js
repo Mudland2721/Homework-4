bc849c9439da929d5c156aae55c492e461206a6b
Mudland2721 <Mudland2721@gmail.com>
2020-04-17 23:45:43 -0600

    Added new html,css, and java

---
+++ questions.js
@@ -0,0 +1,33 @@
+function questions(text, choices, answer) {
+  this.text = text;
+  this.choices = choices;
+  this.answer = answer;
+}
+
+var questions = [
+  //make an array with each item in it will hold a question, then the questions, and the right awnser
+
+  new questions(
+    "What is the best shooter game?",
+    ["COD", "CSGO", "Halo", "DOOM"],
+    "DOOM"
+  ),
+
+  new questions(
+    "Whats the best fighting game?",
+    ["Super Smash Bros", "MK9", "Injustice", "Soul Caliber"],
+    "Super Smash Bros"
+  ),
+
+  new questions(
+    "Whats the best horror game?",
+    ["Resident Evil", "Silent Hill", "Out Last", "5 Nights At Freddies"],
+    "Resident Evil"
+  ),
+
+  new questions(
+    "What the best RPG?",
+    ["Witcher", "Skyrim", "Destiny 2", "Fallout"],
+    "Skyrim"
+  ),
+];
